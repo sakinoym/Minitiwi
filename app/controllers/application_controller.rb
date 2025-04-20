15c34b42e20b2e6c1f35c2af6893d0102edb7356
sakinoym <sakino.ym@gmail.com>
2025-04-20 15:59:39 +0900

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include SessionsHelper
   include Pagy::Backend
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
